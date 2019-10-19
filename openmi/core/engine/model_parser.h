2ef58059f064067b063db9ab6e4b34d067df3d68
zhouyong <zhouyong03@meituan.com>
2019-10-19 17:41:32 +0800

    opti model_parser

--- openmi/core/engine/model_parser.h
+++ openmi/core/engine/model_parser.h
@@ -17,90 +17,109 @@ public:
       int column_id = -1;
       GetAttr(node->attrs(), "col_id", &column_id);
       if (column_id == -1) {
-        bool label = false;
-        GetAttr(node->attrs(), "label", &label);
-        // nn source node. 暂不支持不同NN weight对应不同optimizer
-        if (!label && node->node_info().node_scope == NS_FORWARD) {
-          int node_id = 0;
-          GetAttr(node->attrs(), "node_id", &node_id);
-          CHECK(node_id < 0) 
-            << ", but node_id:" << node_id << ", node name:" << node_name;
-
-          CHECK(schema.find(node_id) == schema.end())
-            << "node id has already exists. node_id:" << node_id 
-            << ", node_name:" << node_name;
-
-          auto model_weight_schema = std::make_shared<proto::internal::ModelWeightSchema>();
-          schema.insert({node_id, model_weight_schema});
-
-          TensorShape shape;
-          GetAttr(node->attrs(), "shape", &shape, false);
-
-          proto::Optimizer optimizer;
-          GetAttr(node->attrs(), "optimizer", &optimizer, false);
-          LOG(INFO) << "node: " << node_name << ", optimizer:\n" << optimizer.DebugString();
-
-          // TODO proto::Optimizer
-          InitModelWeightSchema(shape, node_name, node_id, model_weight_schema.get());
-        }
+        CreateNNWeightSchema(node, schema);
       } else {
-        SourceNodeType source_node_type = proto::X;
-        GetAttr(node->attrs(), "source_node_type", &source_node_type);
-        if (source_node_type != proto::W || 
-            node->node_info().node_scope != NS_FORWARD) {
-          continue;
-        }
-        std::shared_ptr<proto::internal::ModelWeightSchema> model_weight_schema;
-        if (schema.find(column_id) != schema.end()) {
-          model_weight_schema = schema[column_id];
-        } else {
-          model_weight_schema = std::make_shared<proto::internal::ModelWeightSchema>();
-          schema.insert({column_id, model_weight_schema});
-        }
-        int embedding_size = 1;
-        GetAttr(node->attrs(), "embedding_size", &embedding_size);
-        CHECK(embedding_size > 0) << "node '" << node_name 
-          << "' has invalid param 'embedding_size' that value should be greater 0";
-
-        // TODO proto::Optimizer
-        proto::Optimizer optimizer;
-        GetAttr(node->attrs(), "optimizer", &optimizer, false);
-        LOG(INFO) << "node: " << node_name << ", optimizer:\n" << optimizer.DebugString();
-        InitModelWeightSchema(node_name, column_id, model_weight_schema.get(), embedding_size);
+        CreateColumnWeightSchema(node, column_id, schema);
       }
     }
   }
 
-  static void InitModelWeightSchema(std::string& node_name, int id, proto::internal::ModelWeightSchema* schema, int size) {
-    schema->set_column_id(id);
-    auto* weight_schema = schema->add_weight_schema();
-    auto cur_weight_len = schema->weight_len();
-    auto cur_param_len = schema->param_len();
-    weight_schema->set_weight_offset(cur_weight_len);
-    weight_schema->set_weight_size(size);
-    // TODO add optimizer param. 
-    // weight_schema->set_param_size();
-    schema->set_weight_len(cur_weight_len + weight_schema->weight_size());
-    schema->set_param_len(cur_param_len + weight_schema->param_size());
-    LOG(INFO) << __FUNCTION__ << " node_name:" << node_name << ", id:" << id << ", size:" << size 
-      << ", weight_len:" << schema->weight_len() << ", number of weight:" << schema->weight_schema_size();
+  static void CreateColumnWeightSchema(Node* node, int column_id, std::unordered_map<int, std::shared_ptr<proto::internal::ModelWeightSchema> >& schema) {
+    auto node_name = node->def().name();
+    SourceNodeType source_node_type = proto::X;
+    GetAttr(node->attrs(), "source_node_type", &source_node_type);
+    if (source_node_type != proto::W || node->node_info().node_scope != NS_FORWARD) {
+      return;
+    }
+    
+    std::shared_ptr<proto::internal::ModelWeightSchema> column_weight_schema;
+    if (schema.find(column_id) != schema.end()) {
+      column_weight_schema = schema[column_id];
+    } else {
+      column_weight_schema = std::make_shared<proto::internal::ModelWeightSchema>();
+      schema.insert({column_id, column_weight_schema});
+    }
+    int embedding_size = 1;
+    GetAttr(node->attrs(), "embedding_size", &embedding_size);
+    CHECK(embedding_size > 0) << "node '" << node_name 
+      << "' has invalid param 'embedding_size' that value should be greater 0";
+
+    // TODO proto::Optimizer
+    proto::Optimizer optimizer;
+    GetAttr(node->attrs(), "optimizer", &optimizer, false);
+    LOG(INFO) << "node: " << node_name << ", optimizer:\n" << optimizer.DebugString();
+
+    // create schema
+    column_weight_schema->set_column_id(column_id);
+    auto* weight_schema = column_weight_schema->add_weight_schema();
+    auto prev_weight_len = column_weight_schema->weight_len();
+    auto prev_param_len = column_weight_schema->param_len();
+    weight_schema->set_weight_offset(prev_weight_len);
+    weight_schema->set_weight_size(embedding_size);
+    weight_schema->set_weight_bytes(weight_schema->weight_size() * sizeof(float));
+    // TODO 添加optimizer参数
+    weight_schema->set_param_bytes(weight_schema->weight_size() * SizeofOptimizer(optimizer.type()));
+    column_weight_schema->set_weight_len(prev_weight_len + weight_schema->weight_size());
   }
 
-  static void InitModelWeightSchema(TensorShape& shape, std::string& node_name, int id, proto::internal::ModelWeightSchema* schema) {
-    schema->set_column_id(id);
-    auto* weight_schema = schema->add_weight_schema();
-    CHECK(shape.Dims() == 2);
+  static void CreateNNWeightSchema(Node* node, std::unordered_map<int, std::shared_ptr<proto::internal::ModelWeightSchema> >& schema) {
+    bool is_label_node = false;
+    GetAttr(node->attrs(), "label", &is_label_node);
+    if (is_label_node || node->node_info().node_scope != NS_FORWARD) {
+      return;
+    }
+
+    auto node_name = node->def().name();
+    int node_id = 0;
+    GetAttr(node->attrs(), "node_id", &node_id);
+    CHECK(node_id < 0) 
+      << "nn node id must be < 0. but node_id:" << node_id << ", node name:" << node_name;
+    
+    CHECK(schema.find(node_id) == schema.end())
+      << "node id has already exists in schema. node_id:" << node_id << ", node_name:" << node_name;
+     
+    TensorShape shape;
+    GetAttr(node->attrs(), "shape", &shape, false);
+    CHECK(shape.Dims() == 2) 
+      << "nn node shape.dims != 2. shape:" << shape.DebugString();
     auto dim_2nd = shape.DimSize(1);
-    auto cur_weight_len = schema->weight_len();
-    auto cur_param_len = schema->param_len();
-    weight_schema->set_weight_offset(cur_weight_len);
+    
+    proto::Optimizer optimizer;
+    GetAttr(node->attrs(), "optimizer", &optimizer, false);
+
+    // create schema
+    auto nn_weight_schema = std::make_shared<proto::internal::ModelWeightSchema>();
+    schema.insert({node_id, nn_weight_schema});
+    nn_weight_schema->set_column_id(node_id);
+  
+    auto* weight_schema = nn_weight_schema->add_weight_schema();
+    weight_schema->set_weight_offset(0);
     weight_schema->set_weight_size(dim_2nd);
-    // TODO add optimizer param
-    // weight_schema->set_param_size();
-    schema->set_weight_len(cur_weight_len + weight_schema->weight_size());
-    schema->set_param_len(cur_param_len + weight_schema->param_size());
-    LOG(INFO) << __FUNCTION__ << " node_name:" << node_name << ", id:" << id 
-      << ", size:" << dim_2nd << ", weight_len:" << schema->weight_len();
+    weight_schema->set_weight_bytes(weight_schema->weight_size() * sizeof(float));
+    // TODO 添加优化器参数
+    weight_schema->set_param_bytes(weight_schema->weight_size() * SizeofOptimizer(optimizer.type()));
+    nn_weight_schema->set_weight_len(weight_schema->weight_size());
+  }
+
+  static int SizeofOptimizer(openmi::proto::OptimizerType optimizer_type) {
+    int bytes = 0;
+    switch (optimizer_type) {
+      case proto::SGD:
+        bytes = 0;
+        break;
+      case proto::FTRL:
+        bytes = 2 * sizeof(float);
+        break;
+      case proto::ADAGRAD:
+        bytes = 1 * sizeof(float);
+        break; 
+      case proto::ADAM: 
+        bytes = 2 * sizeof(float);
+        break;  
+      default:
+        break;
+    }
+    return bytes;
   }
 }; // class ModelParser
 
